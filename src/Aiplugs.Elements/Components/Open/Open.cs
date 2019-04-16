177666db2f968ad80ef457e1af20695c257eab0b
iwate <iwatesoftware@live.jp>
2019-04-16 23:52:41 +0900

    add open feature

--- src/Aiplugs.Elements/Components/Open/Open.cs
+++ src/Aiplugs.Elements/Components/Open/Open.cs
@@ -8,12 +8,22 @@ namespace Aiplugs.Elements
     {
         public string Open { get; set; }
         public string OpenTo { get; set; } = "aiplugs-blade";
+        public string ReplaceTo { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.Attributes.Add("ic-get-from", Open);
-            output.Attributes.Add("ic-target", OpenTo.ToIcTarget());
-            output.Attributes.Add("ic-swap-style", "append");
+
+            if (string.IsNullOrEmpty(ReplaceTo))
+            {
+                output.Attributes.Add("ic-target", OpenTo.ToIcTarget());
+                output.Attributes.Add("ic-swap-style", "append");
+            }
+            else
+            {
+                output.Attributes.Add("ic-target", ReplaceTo.ToIcTarget());
+                output.Attributes.Add("ic-swap-style", "replace");
+            }
         }
     }
 }
