d0bbd3fe755a24d6a9001ab5982e30d5e5fbde05
iwate <iwatesoftware@live.jp>
2019-04-21 16:09:36 +0900

    add search tag

---
+++ src/Aiplugs.Elements/Components/Search/Search.cs
@@ -0,0 +1,59 @@
+﻿using Aiplugs.Elements.Extensions;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+
+namespace Aiplugs.Elements.Components.Search
+{
+    [HtmlTargetElement("aiplugs-search")]
+    public class Search : TagHelper
+    {
+        public IEnumerable<SelectListItem> MethodList { get; set; }
+        public string MethodName { get; set; } = "method";
+        public string Name { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.Merge("class", "aiplugs-search");
+
+            if (MethodList != null)
+            {
+                output.Tag("select", () =>
+                {
+                    output.Attr("class", "aiplugs-serach__method");
+                }, () =>
+                {
+                    foreach (var item in MethodList)
+                    {
+                        output.Tag("option", () =>
+                        {
+                            output.Attr("value", item.Value);
+
+                            if (item.Selected)
+                                output.Attr("selected");
+
+                        }, item.Text);
+                    }
+                });
+            }
+
+            output.Tag("input", () =>
+            {
+                output.Attr("name", Name);
+                output.Attr("class", "aiplugs-search__query");
+            });
+
+            output.Tag("button", () =>
+            {
+                output.Attr("type", "submit");
+                output.Attr("class", "aiplugs-search__submit");
+            }, () =>
+            {
+                output.Html("<i class=\"fa fa-search\"></i>");
+            });
+        }
+    }
+}
