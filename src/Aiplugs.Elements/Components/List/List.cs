0d166ff7efa79fb55af9a6e0244fa05ca744b26a
iwate <iwatesoftware@live.jp>
2019-08-12 17:07:45 +0900

    modify list label

--- src/Aiplugs.Elements/Components/List/List.cs
+++ src/Aiplugs.Elements/Components/List/List.cs
@@ -12,6 +12,8 @@ namespace Aiplugs.Elements
     public class AiplugsList : TagHelper
     {
         public Dictionary<string, string> Labels { get; set; } 
+        public Dictionary<string, string> LabelClasses { get; set; } 
+        public Dictionary<string, string> LabelStyles { get; set; } 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (Labels == null) 
@@ -19,6 +21,12 @@ namespace Aiplugs.Elements
                 Labels = context.AllAttributes
                                 .Where(attr => attr.Name.StartsWith("label-"))
                                 .ToDictionary(attr => attr.Name.Replace("label-",""), attr => ((HtmlString)attr.Value).Value);
+                LabelClasses = context.AllAttributes
+                                .Where(attr => attr.Name.StartsWith("class-"))
+                                .ToDictionary(attr => attr.Name.Replace("class-",""), attr => ((HtmlString)attr.Value).Value);
+                LabelStyles = context.AllAttributes
+                                .Where(attr => attr.Name.StartsWith("style-"))
+                                .ToDictionary(attr => attr.Name.Replace("style-",""), attr => ((HtmlString)attr.Value).Value);
             }
             
             var content = await output.GetChildContentAsync();
@@ -28,12 +36,14 @@ namespace Aiplugs.Elements
             output.Attributes.Add("data-controller", "aiplugs-list");
 
             var labels = string.Join("", Labels.Select(kv => {
-                string text = kv.Value, @class = "";
-                if (text.EndsWith("*")) {
-                    @class = "class=\"flex\"";
-                    text = text.Substring(0, text.Length - 1);
+                string text = kv.Value, @class = string.Empty, style = string.Empty;
+                if (LabelClasses.ContainsKey(kv.Key)) {
+                    @class = $"class=\"{LabelClasses[kv.Key]}\"";
                 }
-                return $"<th {@class}>{text}</th>";
+                if (LabelStyles.ContainsKey(kv.Key)) {
+                    style = $"style=\"{LabelStyles[kv.Key]}\"";
+                }
+                return $"<th {@class} {style}>{text}</th>";
             }));
             output.Content.AppendHtml($"<thead><th></th>{labels}</thread>");
             output.Content.AppendHtml("<tbody>");
